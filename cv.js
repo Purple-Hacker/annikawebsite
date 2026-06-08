322e9535f940c1e8b94ad97dbe7eef95bf0c5c14
annikasalmi <annikaksalmi@gmail.com>
2026-06-08 14:45:35 +0200

    updating pictures, etc.

--- cv.js
+++ cv.js
@@ -161,12 +161,12 @@ const PAPERS_CONTENT = [
     "<em>The Role of Formation Location in Shaping Sulfur-, Nitrogen-, and Carbon-Bearing Species in Super-Earth and Sub-Neptune Atmospheres</em>, accepted ([<a href=\"https://arxiv.org/abs/2605.15170\" target=\"_blank\" rel=\"noopener noreferrer\">arXiv</a>]).<br>\
     A. Werlen, R. Burn, C. Dorn, L. Felix, <strong>A. Salmi</strong>",
 
+    "<em>The GAPS Programme at TNG. LXXIII. Confirmation of the hot sub-Neptune TOI-4602 b (HD 25295 b), a key target for future atmospheric characterization</em>, accepted ([<a href=\"https://arxiv.org/abs/2604.11728\" target=\"_blank\" rel=\"noopener noreferrer\">arXiv</a>]).<br>\
+    C. Di Maio, S. Benatti, D. Locci, R. Spinelli, M. Baratella, K. Biazzo, J. Maldonado, A. F. Lanza, C. Dorn, P. E. Cubillos, <strong>A. Salmi</strong>...",
+
     "<em>Revisiting TOI-4438 and TOI-442 planetary systems with new observations from SPIRou and TESS</em>, submitted.<br>J. \
     Bell, G. Hébrard, E. Martioli, R. Díaz, L. de Almeida, R. Doyon, D. de Oliveira, A. L'Heureux, É. Artigau, L. Arnold, \
     I. Boisse, X. Bonfils, A. Carmona, N. J. Cook, X. Delfosse, J.-F. Donati, <strong>A. Salmi</strong>, M. Valatsou",
-
-    "<em>The GAPS Programme at TNG. LXXIII. Confirmation of the hot sub-Neptune TOI-4602 b (HD 25295 b), a key target for future atmospheric characterization</em>, accepted ([<a href=\"https://arxiv.org/abs/2604.11728\" target=\"_blank\" rel=\"noopener noreferrer\">arXiv</a>]).<br>\
-    C. Di Maio, S. Benatti, D. Locci, R. Spinelli, M. Baratella, K. Biazzo, J. Maldonado, A. F. Lanza, C. Dorn, P. E. Cubillos, <strong>A. Salmi</strong>...",
 ];
 
 // Function to render time table entries
